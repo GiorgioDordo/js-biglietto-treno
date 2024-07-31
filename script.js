512ba68c06d9746b888059b9a312fa1bd1673955
GiorgioDordo <djole6789@gmail.com>
2024-07-31 18:46:30 +0200

    exercise completed

--- script.js
+++ script.js
@@ -9,6 +9,28 @@ const howMuchKm = parseInt(prompt('Quanti chilometri vuoi percorrere?', 56));
 const age = parseInt(prompt('Quanti anni hai?', 20));
 console.log('howMuchKm', howMuchKm);
 console.log('age', age);
+const priceTicket = howMuchKm * 0.21;
+if(isNaN(howMuchKm)  || isNaN(age)) {
+    alert('Dati non inseriti correttamente');
+} else {
 
-let priceTicket = howMuchKm * pricePerKm;
-console.log('priceTicket', priceTicket);
+    let finalPrice = priceTicket;
+    if (age >= 18) {
+        finalPrice = priceTicket;
+        console.log('finalPrice', finalPrice);
+        resultElement.innerHTML = `Il prezzo è: <strong>${finalPrice}€<strong>`;
+    } else if (age < 18) {
+        finalPrice = priceTicket * 0.8;
+        console.log('finalPrice', finalPrice);
+        resultElement.innerHTML = `Congratulazioni hai ricevuto uno sconto del 20%. Il prezzo è <strong>${finalPrice}€<strong>`;
+    } else if (age >= 65) {
+        finalPrice = priceTicket * 0.6;
+        console.log('finalPrice', finalPrice);
+        resultElement.innerHTML = `Congratulazioni hai ricevuto uno sconto del 40%. Il prezzo è <strong>${finalPrice}€<strong>`;
+    }
+}
+
+
+
+
+
