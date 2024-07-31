e50ecf170b97d27b0a62e9a5fd310db738990bb0
GiorgioDordo <djole6789@gmail.com>
2024-07-31 15:02:04 +0200

    asked user's age and km to do

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log('JS OK');
+
+const resultElement = document.getElementById('result');
+console.log(resultElement);
+
+const howMuchKm = parseInt(prompt('Quanti chilometri vuoi percorrere?', 56));
+const age = parseInt(prompt('Quanti anni hai?', 20));
+console.log('howMuchKm', howMuchKm);
+console.log('age', age);
