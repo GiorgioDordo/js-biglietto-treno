a3d5ca5891b9a47bc6e5a3d831b8ff30c8f0f5ac
GiorgioDordo <djole6789@gmail.com>
2024-07-31 15:08:16 +0200

    calculated ticket price

--- script.js
+++ script.js
@@ -3,7 +3,12 @@ console.log('JS OK');
 const resultElement = document.getElementById('result');
 console.log(resultElement);
 
+const pricePerKm = 0.21;
+
 const howMuchKm = parseInt(prompt('Quanti chilometri vuoi percorrere?', 56));
 const age = parseInt(prompt('Quanti anni hai?', 20));
 console.log('howMuchKm', howMuchKm);
 console.log('age', age);
+
+let priceTicket = howMuchKm * pricePerKm;
+console.log('priceTicket', priceTicket);
